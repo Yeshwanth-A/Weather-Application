043eb5e62aad3d8def463bc33551309dcfc43943
Yeshwanth-A <yeshwanth2822@gmail.com>
2023-11-04 16:46:00 +0530

    "fix/weather-application"

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -14,7 +14,7 @@ const Forecast =({data})=>{
   const forecastDays = WEEK_DAYS.slice(dayInAWeek , WEEK_DAYS.length).concat(WEEK_DAYS.slice(0,dayInAWeek));
   return(
     <>
-    <label className="title">Daily</label>
+    <label className="title">Forecast</label>
     <Accordion allowZeroExpanded>
      {data.list.splice(0,7).map((item,idx)=>(
       <AccordionItem key={idx}>
